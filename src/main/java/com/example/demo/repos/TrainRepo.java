382fcee324ff6479841640c8b1da8b173d2fad97
veronikakruchok <kruchok.veronika@gmail.com>
2023-04-24 20:37:11 +0300

    update train repo

--- src/main/java/com/example/demo/repos/TrainRepo.java
+++ src/main/java/com/example/demo/repos/TrainRepo.java
@@ -1,19 +1,24 @@
 package com.example.demo.repos;
 
 import com.example.demo.domain.Train;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface TrainRepo extends JpaRepository<Train, Long> {
-    @Query("SELECT t FROM Train t WHERE t.trainNumber LIKE %:query%")
-    List<Train> searchByTrainNumber(@Param("query") String query);
+public interface TrainRepo extends PagingAndSortingRepository<Train, Long>{
 
-    @Query("SELECT t FROM Train t WHERE t.trainName LIKE %:query%")
-    List<Train> searchByTrainName(@Param("query") String query);
+    @Query("SELECT t FROM Train t WHERE LOWER(t.trainName) " + "LIKE LOWER(CONCAT('%', ?1, '%')) OR LOWER(t.trainNumber) " + "LIKE LOWER(CONCAT('%', ?1, '%'))")
+    Page<Train> findAll(String query, Pageable pageable);
 
+    @Override
+    Iterable<Train> findAll(Sort sort);
 }
